f020ddb2ec1c9c30114a53a1d7f625fb194c45fd
rmckenzie <rmckenzie92@gmail.com>
2012-08-19 03:53:00 -0500

    Write the lein bindngs for the bumpkin internals

--- src/leiningen/bumpkin.clj
+++ src/leiningen/bumpkin.clj
@@ -1,27 +1,34 @@
-(ns leinigen.bumpkin
+(ns leiningen.bumpkin
   (:use bumpkin.core))
 
 (declare bumpkin release build patch set)
 
 (defn project-file [project]
-  (clojure.java.io/File. (str (:root project) "/project.clj")))
+  (clojure.java.io/file (str (:root project) "/project.clj")))
 
 (def bump bumpkin)
 
 (defn bumpkin 
   "A tool for incrementing the version number
    Arguments:
-       ([release]  - increments the version by 1.0.0 resetting build and patch
-        [build]    - increments the version by 0.1.0 resetting patch
-        [patch]    - increments patch
-        [set]    - forcibly sets the version number to some value
-        [codename]
+       ([major]          - increments the version by 1.0.0 resetting minor and patch
+        [minor]          - increments the version by 0.1.0 resetting patch
+        [patch]          - increments patch
+        [set <VERSION>]  - forcibly sets the version number to some value
        )"
   ^{:subtasks [release build patch force]} 
   [project & args]
-  (patch project))
+  (update-version-number (project-file project) patch-inc))
 
-(defn release)
-(defn build)
-(defn patch)
-(defn set)
+(defn major [project]
+  (update-version-number (project-file project) major-inc))
+
+(defn minor [project] 
+  (update-version-number (project-file project) minor-inc))
+
+(defn patch [project]
+  (update-version-number (project-file project) patch-inc))
+
+(defn set [project version]
+  (let [file (project-file project)]
+    (update-version file (slurp file) (parse-version version))))
