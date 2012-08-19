08a52ab72dfdb6069c6c6d43b8f4af1f28545640
rmckenzie <rmckenzie92@gmail.com>
2012-08-19 02:39:35 -0500

    Bring project up to date

--- project.clj
+++ project.clj
@@ -1,6 +1,7 @@
-(defproject bumpkin "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+(defproject bumpkin "0.2.1-SNAPSHOT"
+  :description "A (not so simple) bump program in Clojure"
+  :url "http://github.com/rdmckenzie/bumpkin"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.4.0"]])
+  :dependencies [[org.clojure/clojure "1.4.0"]
+                 [org.clojure/tools.cli "0.2.2"]])
